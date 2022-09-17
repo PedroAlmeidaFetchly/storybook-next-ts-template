a3f7a3f37819824da76d363e7e3644002bb91db0
PedroAlmeidaFetchly <107511028+PedroAlmeidaFetchly@users.noreply.github.com>
2022-09-17 18:54:03 -0300

    mudanças do Matheus Lopes

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,13 @@
 import * as React from "react";
+import ButtonWithSass from "src/components/ButtonWithSass";
 
 const Home = () => {
-  return <h1>Welcome to My Next App!</h1>;
+  return (
+    <>
+      <h1>Welcome to My Next App!</h1>
+      <ButtonWithSass />
+    </>
+  );
 };
 
 export default Home;
---
+++ src/components/ButtonWithSass/index.tsx
@@ -0,0 +1,9 @@
+import styles from "ButtonWithSass.module.scss";
+
+const ButtonWithSass = () => (
+  <div id={styles.root}>
+    <button>Button</button>
+  </div>
+);
+
+export default ButtonWithSass;
